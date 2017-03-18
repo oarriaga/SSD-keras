0d58371a06695bdf2eed32b8a30bba3d1a1a20aa
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-03-18 10:30:38 +0100

    Found prior_box_manger.assign_boxes bug for which assigns several classes to a same prior box

--- src/test.py
+++ src/test.py
@@ -1,21 +1,21 @@
-from keras.models import load_model
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
 
+from models import my_SSD
 from utils.utils import read_image
 from utils.utils import resize_image
 
-
-
-model_filename = '../trained_models/model_checkpoints/weights.09-0.25.hdf5'
-model = load_model(model_filename)
+model = my_SSD(num_classes=21)
+weights_filename = '../trained_models/model_checkpoints/weights.hdf5'
+model.load_weights(weights_filename)
 image_size = model.input_shape[1:3]
-
 image_filename = '../images/008745.jpg'
 image_array = read_image(image_filename)
 image_array = resize_image(image_array, image_size)
+image_array = image_array.astype('float32')
 image_array = np.expand_dims(image_array, 0)
 image_array = preprocess_input(image_array)
 predictions = model.predict([image_array])
+classification = np.squeeze(predictions[1])
 
 
