Incorrect dimensions given to the max function

wtf/my_multibox_loss.py
@@ -73,7 +73,7 @@ class MultiboxLoss(object):
         # picking up the negative examples with the highest probability (highest loss)
         ### ?????? THIS IS WEIRD, the original implementation starts from 5: therefore it
         #### does not take into consideration the background boxes
-        pred_class_values = K.max(y_pred_classification[1:], axis=2)
+        pred_class_values = K.max(y_pred_classification[:, :, 1:], axis=2)
         int_negatives_mask = y_true[:, :, 4 + self.background_id]
         pred_negative_class_values = pred_class_values * int_negatives_mask
         top_k_negative_indices = tf.nn.top_k(pred_negative_class_values, k=num_neg_batch)[1]