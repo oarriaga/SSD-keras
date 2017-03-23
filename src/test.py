9da181d5d855b71f2448728ac8275ee57b7b878c
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-03-23 20:18:56 +0100

    Modified box_manager to use the current in-built loss function

--- src/test.py
+++ src/test.py
@@ -1,11 +1,11 @@
-from keras.applications.vgg16 import preprocess_input
 import numpy as np
 
 from ssd import SSD300
 from utils.utils import read_image
 from utils.utils import resize_image
+from utils.utils import preprocess_image
 
-model = SSD300((300,300,3))
+model = SSD300()
 weights_filename = '../trained_models/model_checkpoints/weights.hdf5'
 model.load_weights(weights_filename)
 image_size = model.input_shape[1:3]
@@ -14,10 +14,11 @@ image_array = read_image(image_filename)
 image_array = resize_image(image_array, image_size)
 image_array = image_array.astype('float32')
 image_array = np.expand_dims(image_array, 0)
-image_array = preprocess_input(image_array)
+image_array = preprocess_image(image_array)
 predictions = model.predict([image_array])
 predictions = np.squeeze(predictions)
-classification = predictions[: , 4:]
-best_classes = np.argmax(classification, axis=1)
+predicted_classes = predictions[: , 4:]
+best_classes = np.argmax(predicted_classes, axis=1)
 positive_mask = best_classes != 0
+
 
