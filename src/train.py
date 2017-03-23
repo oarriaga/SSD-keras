3b62453c440b461feb84e1ad6a687b6a183283d5
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-03-23 09:36:00 +0100

    added box manager for testint purposes

--- src/train.py
+++ src/train.py
@@ -3,23 +3,18 @@ from keras.callbacks import LearningRateScheduler
 from keras.optimizers import Adam
 
 from image_generator import ImageGenerator
+from multibox_loss import MultiboxLoss
 from ssd import SSD300
-#from models import my_SSD
-#from multibox_loss import MultiboxLoss
 from utils.prior_box_creator import PriorBoxCreator
 from utils.prior_box_manager import PriorBoxManager
-#from ssd_utils import BBoxUtility
-from ssd_training import MultiboxLoss
 from utils.XML_parser import XMLParser
 from utils.utils import split_data
 from utils.utils import scheduler
-from utils.utils import flatten_prior_boxes
 
-batch_size = 5
+batch_size = 7
 num_epochs = 15
 num_classes = 21
-#model = my_SSD(num_classes)
-model = SSD300((300,300,3))
+model = SSD300()
 model.load_weights('../trained_models/weights_SSD300.hdf5', by_name=True)
 freeze = ['input_1', 'conv1_1', 'conv1_2', 'pool1',
           'conv2_1', 'conv2_2', 'pool2',
@@ -34,9 +29,10 @@ box_creator = PriorBoxCreator(model)
 prior_boxes = box_creator.create_boxes()
 
 root_prefix = '../datasets/VOCdevkit/VOC2007/'
+ground_data_prefix = root_prefix + 'Annotations/'
 image_prefix = root_prefix + 'JPEGImages/'
 
-ground_data_prefix = root_prefix + 'Annotations/'
+
 ground_truth_manager = XMLParser(ground_data_prefix, background_id=0)
 ground_truth_data = ground_truth_manager.get_data()
 
@@ -45,8 +41,6 @@ train_keys, validation_keys = split_data(ground_truth_data, training_ratio=.8)
 prior_box_manager = PriorBoxManager(prior_boxes,
                                     box_scale_factors=[.1, .1, .2, .2])
 
-prior_boxes = flatten_prior_boxes(prior_boxes)
-#bbox_util = BBoxUtility(21, prior_boxes)
 image_generator = ImageGenerator(ground_truth_data,
                                  prior_box_manager,
                                  #bbox_util,
@@ -57,7 +51,7 @@ image_generator = ImageGenerator(ground_truth_data,
                                  vertical_flip_probability=0,
                                  horizontal_flip_probability=0.5)
 
-multibox_loss = MultiboxLoss(num_classes, neg_pos_ratio=2.0).compute_loss
+multibox_loss = MultiboxLoss(num_classes, neg_pos_ratio=3.0).compute_loss
 model.compile(optimizer=Adam(lr=3e-4), loss=multibox_loss, metrics=['acc'])
 model_names = ('../trained_models/model_checkpoints/' +
                'weights.{epoch:02d}-{val_loss:.2f}.hdf5')
@@ -67,8 +61,6 @@ model_checkpoint = ModelCheckpoint(model_names,
                                    save_best_only=False,
                                    save_weights_only=True)
 learning_rate_schedule = LearningRateScheduler(scheduler)
-
-
 model.fit_generator(image_generator.flow(mode='train'),
                     len(train_keys),
                     num_epochs,
