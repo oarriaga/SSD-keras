14b970f97dbe375d46fa65da90dd25b964fa6298
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-03-29 18:54:48 +0200

    Added new model

--- src/video_test/videotest_example.py
+++ src/video_test/videotest_example.py
@@ -2,18 +2,21 @@ from videotest import VideoTest
 
 import sys
 sys.path.append("..")
-from models import SSD300 as SSD
+from mini_models import mini_SSD300
 
 input_shape = (300,300,3)
 
 # Change this if you run with other classes than VOC
-class_names = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"];
+class_names=['chair', 'bottle', 'sofa', 'tvmonitor', 'diningtable']
+
 NUM_CLASSES = len(class_names)
 
-model = SSD(input_shape, num_classes=NUM_CLASSES)
+num_classes = len(class_names) + 1
+
+model = mini_SSD300(input_shape, num_classes)
 
 # Change this path if you want to use your own trained weights
-model.load_weights('../../trained_models/weights_SSD300.hdf5')
+model.load_weights('../../trained_models/minh.hdf5')
 vid_test = VideoTest(class_names, model, input_shape)
 
 # To test on webcam 0, remove the parameter (or change it to another number
