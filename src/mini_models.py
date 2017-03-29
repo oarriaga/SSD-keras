65c2980aed9dbf9d49ca5eb19199152c39d86e2a
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-03-29 20:12:26 +0200

    Modified the model to add more prior boxes

--- src/mini_models.py
+++ src/mini_models.py
@@ -13,17 +13,7 @@ from layers import Normalize
 from layers import PriorBox
 
 
-def mini_SSD300(input_shape=(300,300,3), num_classes=21):
-    """SSD300 architecture.
-
-    # Arguments
-        input_shape: Shape of the input image,
-            expected to be either (300, 300, 3) or (3, 300, 300)(not tested).
-        num_classes: Number of classes including background.
-
-    # References
-        https://arxiv.org/abs/1512.02325
-    """
+def mini_SSD300(input_shape=(300, 300, 3), num_classes=21):
     net = {}
     # Block 1
     input_tensor = input_tensor = Input(shape=input_shape)
@@ -65,52 +55,9 @@ def mini_SSD300(input_shape=(300,300,3), num_classes=21):
                                    name='conv3_3')(net['conv3_2'])
     net['pool3'] = MaxPooling2D((2, 2), strides=(2, 2), border_mode='same',
                                 name='pool3')(net['conv3_3'])
-    """
-    # Block 4
-    net['conv4_1'] = Convolution2D(512, 3, 3,
-                                   activation='relu',
-                                   border_mode='same',
-                                   name='conv4_1')(net['pool3'])
-    net['conv4_2'] = Convolution2D(512, 3, 3,
-                                   activation='relu',
-                                   border_mode='same',
-                                   name='conv4_2')(net['conv4_1'])
-    net['conv4_3'] = Convolution2D(512, 3, 3,
-                                   activation='relu',
-                                   border_mode='same',
-                                   name='conv4_3')(net['conv4_2'])
-    net['pool4'] = MaxPooling2D((2, 2), strides=(2, 2), border_mode='same',
-                                name='pool4')(net['conv4_3'])
-    # Block 5
-    net['conv5_1'] = Convolution2D(512, 3, 3,
-                                   activation='relu',
-                                   border_mode='same',
-                                   name='conv5_1')(net['pool4'])
-    net['conv5_2'] = Convolution2D(512, 3, 3,
-                                   activation='relu',
-                                   border_mode='same',
-                                   name='conv5_2')(net['conv5_1'])
-    net['conv5_3'] = Convolution2D(512, 3, 3,
-                                   activation='relu',
-                                   border_mode='same',
-                                   name='conv5_3')(net['conv5_2'])
-    net['pool5'] = MaxPooling2D((3, 3), strides=(1, 1), border_mode='same',
-                                name='pool5')(net['conv5_3'])
-    # FC6
-    net['fc6'] = AtrousConvolution2D(1024, 3, 3, atrous_rate=(6, 6),
-                                     activation='relu', border_mode='same',
-                                     name='fc6')(net['pool5'])
-    # x = Dropout(0.5, name='drop6')(x)
-    # FC7
-    net['fc7'] = Convolution2D(1024, 1, 1, activation='relu',
-                               border_mode='same', name='fc7')(net['fc6'])
-    # x = Dropout(0.5, name='drop7')(x)
-    # Block 6
-    # deleted
-    """
 
     net['conv4_3_norm'] = Normalize(20, name='conv4_3_norm')(net['conv3_3'])
-    num_priors = 3
+    num_priors = 6
     x = Convolution2D(num_priors * 4, 3, 3, border_mode='same',
                       name='conv4_3_norm_mbox_loc')(net['conv4_3_norm'])
     net['conv4_3_norm_mbox_loc'] = x
@@ -124,39 +71,12 @@ def mini_SSD300(input_shape=(300,300,3), num_classes=21):
     net['conv4_3_norm_mbox_conf'] = x
     flatten = Flatten(name='conv4_3_norm_mbox_conf_flat')
     net['conv4_3_norm_mbox_conf_flat'] = flatten(net['conv4_3_norm_mbox_conf'])
-    priorbox = PriorBox(img_size, 30.0, aspect_ratios=[2],
+    priorbox = PriorBox(img_size, 30.0, max_size=60, aspect_ratios=[2, 3],
                         variances=[0.1, 0.1, 0.2, 0.2],
                         name='conv4_3_norm_mbox_priorbox')
     net['conv4_3_norm_mbox_priorbox'] = priorbox(net['conv4_3_norm'])
 
     # Prediction from fc7
-    """
-    num_priors = 6
-    net['fc7_mbox_loc'] = Convolution2D(num_priors * 4, 3, 3,
-                                        border_mode='same',
-                                        name='fc7_mbox_loc')(net['fc7'])
-    flatten = Flatten(name='fc7_mbox_loc_flat')
-    net['fc7_mbox_loc_flat'] = flatten(net['fc7_mbox_loc'])
-    name = 'fc7_mbox_conf'
-    if num_classes != 21:
-        name += '_{}'.format(num_classes)
-    net['fc7_mbox_conf'] = Convolution2D(num_priors * num_classes, 3, 3,
-                                         border_mode='same',
-                                         name=name)(net['fc7'])
-    flatten = Flatten(name='fc7_mbox_conf_flat')
-    net['fc7_mbox_conf_flat'] = flatten(net['fc7_mbox_conf'])
-    priorbox = PriorBox(img_size, 60.0, max_size=114.0, aspect_ratios=[2, 3],
-                        variances=[0.1, 0.1, 0.2, 0.2],
-                        name='fc7_mbox_priorbox')
-    net['fc7_mbox_priorbox'] = priorbox(net['fc7'])
-    """
-    # Gather all predictions
-    """
-    net['mbox_loc'] = merge([net['conv4_3_norm_mbox_loc_flat']],
-                            mode='concat', concat_axis=1, name='mbox_loc')
-    net['mbox_conf'] = merge([net['conv4_3_norm_mbox_conf_flat']],
-                             mode='concat', concat_axis=1, name='mbox_conf')
-    """
     if hasattr(net['conv4_3_norm_mbox_loc_flat'], '_keras_shape'):
         num_boxes = net['conv4_3_norm_mbox_loc_flat']._keras_shape[-1] // 4
     elif hasattr(net['mbox_loc'], 'int_shape'):
@@ -167,11 +87,6 @@ def mini_SSD300(input_shape=(300,300,3), num_classes=21):
                                name='mbox_conf_logits')(net['conv4_3_norm_mbox_conf_flat'])
     net['mbox_conf'] = Activation('softmax',
                                   name='mbox_conf_final')(net['mbox_conf'])
-    """
-    net['mbox_priorbox'] = merge([net['conv4_3_norm_mbox_priorbox']],
-                                 mode='concat', concat_axis=1,
-                                 name='mbox_priorbox')
-    """
 
     net['predictions'] = merge([net['mbox_loc'],
                                net['mbox_conf'],
