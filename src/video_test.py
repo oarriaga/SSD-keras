35a28b5cc2c5e86d91094e8529799f730d6ab219
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-05-23 15:07:13 +0200

    Added video test

--- src/video_test.py
+++ src/video_test.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from utils.utils import get_class_names
+from utils.utils import preprocess_images
+import cv2
 
 class VideoTest(object):
     def __init__(self, prior_boxes, box_scale_factors=[.1, .1, .2, .2],
@@ -18,6 +20,7 @@ class VideoTest(object):
         self.colors = plt.cm.hsv(np.linspace(0, 1, self.num_classes)).tolist()
         self.arg_to_class = dict(zip(list(range(self.num_classes)),
                                                 self.class_names))
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
 
     def _filter_boxes(self, predictions):
         predictions = np.squeeze(predictions)
@@ -98,7 +101,8 @@ class VideoTest(object):
         image_size = (image_size[1], image_size[0])
         figure, axis = plt.subplots(1)
         axis.imshow(image_array)
-        original_coordinates = self._denormalize_box(box_coordinates, image_size)
+        original_coordinates = self._denormalize_box(box_coordinates,
+                                                            image_size)
         x_min = original_coordinates[:, 0]
         y_min = original_coordinates[:, 1]
         x_max = original_coordinates[:, 2]
@@ -126,10 +130,71 @@ class VideoTest(object):
                       bbox={'facecolor':color, 'alpha':0.5, 'pad':10})
         plt.show()
 
+    def _draw_normalized_box_2(self, box_coordinates, original_image_array):
+        image_array = np.squeeze(original_image_array)
+        image_array = image_array.astype('uint8')
+        image_size = image_array.shape[0:2]
+        image_size = (image_size[1], image_size[0])
+        figure, axis = plt.subplots(1)
+        axis.imshow(image_array)
+        original_coordinates = self._denormalize_box(box_coordinates,
+                                                            image_size)
+        x_min = original_coordinates[:, 0]
+        y_min = original_coordinates[:, 1]
+        x_max = original_coordinates[:, 2]
+        y_max = original_coordinates[:, 3]
+        width = x_max - x_min
+        height = y_max - y_min
+        classes = box_coordinates[:, 4:]
+        num_boxes = len(box_coordinates)
+        for box_arg in range(num_boxes):
+            x_min_box = int(x_min[box_arg])
+            y_min_box = int(y_min[box_arg])
+            box_width = int(width[box_arg])
+            box_height = int(height[box_arg])
+            box_class = classes[box_arg]
+            label_arg = np.argmax(box_class)
+            score = box_class[label_arg]
+            class_name = self.arg_to_class[label_arg]
+            color = self.colors[label_arg]
+            display_text = '{:0.2f}, {}'.format(score, class_name)
+            cv2.rectangle(original_image_array, (x_min_box, y_min_box),
+                        (x_min_box + box_width, y_min_box + box_height),
+                                                                color, 2)
+            cv2.putText(original_image_array, display_text,
+                        (x_min_box, y_min_box - 30), self.font,
+                        .7, color, 1, cv2.LINE_AA)
+
+    def draw_boxes_in_video(self, predictions, original_image_array):
+        decoded_predictions = self._decode_boxes(predictions)
+        selected_boxes = self._filter_boxes(decoded_predictions)
+        self._draw_normalized_box_2(selected_boxes, original_image_array)
+
     def draw_boxes(self, predictions, original_image_array):
         decoded_predictions = self._decode_boxes(predictions)
         selected_boxes = self._filter_boxes(decoded_predictions)
         self._draw_normalized_box(selected_boxes, original_image_array)
+
+    def start_video(self, model):
+        camera = cv2.VideoCapture(0)
+        while True:
+            ret, frame = camera.read()
+            image_array = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image_array = image_array.astype('float32')
+            image_array = cv2.resize(image_array, (300, 300))
+            image_array = np.expand_dims(image_array, axis=0)
+            image_array = preprocess_images(image_array)
+            predictions = model.predict(image_array)
+            predictions = np.squeeze(predictions)
+            self.draw_boxes_in_video(predictions, frame)
+            cv2.imshow('webcam', frame)
+            if cv2.waitKey(1)&0xFF == ord('q'):
+                break
+
+        camera.release()
+        cv2.destroyAllWindows()
+
+
 
 
 if __name__ == "__main__":
@@ -146,58 +211,16 @@ if __name__ == "__main__":
     model.load_weights(weights_filename)
 
     #loading image
-    image_filename = 'test_resources/fish-bike.jpg'
-    original_image_array = imread(image_filename)
-    image_array = load_image(image_filename, target_size=(300, 300))
-    image_array = np.expand_dims(image_array, axis=0)
-    predictions = model.predict(image_array)
-    predictions = np.squeeze(predictions)
+    #image_filename = 'test_resources/fish-bike.jpg'
+    #original_image_array = imread(image_filename)
+    #image_array = load_image(image_filename, target_size=(300, 300))
+    #image_array = np.expand_dims(image_array, axis=0)
+    #image_array = preprocess_images(image_array)
+    #predictions = model.predict(image_array)
+    #predictions = np.squeeze(predictions)
 
     # testing class
     video = VideoTest(prior_boxes)
-    video.draw_boxes(predictions, original_image_array)
+    #video.draw_boxes(predictions, original_image_array)
 
-"""
-if __name__ == "__main__":
-    from models import SSD300
-    from utils.utils import load_image
-    from utils.visualizer import Visualizer
-    from utils.utils import get_class_names
-    from utils.prior_box_creator import PriorBoxCreator
-    from utils.prior_box_manager import PriorBoxManager
-    from scipy.misc import imread
-    model = SSD300()
-    box_creator = PriorBoxCreator(model)
-    prior_boxes = box_creator.create_boxes()
-    box_manager = PriorBoxManager(prior_boxes, box_scale_factors=[.1, .1, .2, .2])
-    weights_filename = '../trained_models/ssd300_weights.34-1.54.hdf5'
-    model.load_weights(weights_filename)
-    image_filename = 'test_resources/fish-bike.jpg'
-
-    image = imread(image_filename)
-    image_size = image.shape[0:2]
-    image_array = load_image(image_filename, target_size=(300, 300))
-    image_array = np.expand_dims(image_array, axis=0)
-    predictions = model.predict(image_array)
-    #selected_boxes = detect_image(predictions)
-    class_names = get_class_names('VOC2007')
-    num_classes = len(class_names)
-    background_index = 0
-    lower_bound = 0.9
-    predictions = np.squeeze(predictions)
-    predictions = box_manager.decode_boxes(predictions)
-
-    encoded_coordinates = predictions[:, :4]
-    box_classes = predictions[:, 4:(4 + num_classes)]
-    best_classes = np.argmax(box_classes, axis=-1)
-    best_probabilities = np.max(box_classes, axis=-1)
-    background_mask = best_classes != background_index
-    lower_bound_mask = lower_bound < best_probabilities
-    mask = np.logical_and(background_mask, lower_bound_mask)
-    selected_boxes = predictions[mask, :(4 + num_classes)]
-
-    arg_to_class = dict(zip(list(range(num_classes)), class_names))
-    box_visualizer = Visualizer(arg_to_class=arg_to_class)
-    box_visualizer.draw_normalized_box(selected_boxes, image)
-"""
-
+    video.start_video(model)
