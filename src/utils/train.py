5c322239f73b657d1f460085bca76f3c53e72df6
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-22 10:05:18 +0200

    Created a train utils file and moved the loss calculation function there

--- src/utils/train.py
+++ src/utils/train.py
@@ -88,3 +88,13 @@ class MultiboxLoss(object):
         positive_localization_loss = positive_localization_loss / num_positives
         total_loss = total_loss + positive_localization_loss
         return total_loss
+
+def scheduler(epoch, decay=0.9, base_learning_rate=3e-4):
+    return base_learning_rate * decay**(epoch)
+
+def split_data(ground_truths, training_ratio=.8):
+    ground_truth_keys = sorted(ground_truths.keys())
+    num_train = int(round(training_ratio * len(ground_truth_keys)))
+    train_keys = ground_truth_keys[:num_train]
+    validation_keys = ground_truth_keys[num_train:]
+    return train_keys, validation_keys
