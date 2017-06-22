a065e387123a54e6395e3df45217f80e003c9416
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-22 18:17:25 +0200

    Rolled back to keras preprocessing tools instead of calling explicitly PIL

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,7 +1,18 @@
 import numpy as np
 from PIL import Image as pil_image
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing import image as keras_image_preprocessor
 
-def preprocess_images(image_array, backend='tensorflow'):
+def preprocess_images(image_array):
+    return preprocess_input(image_array)
+
+def load_image(image_path, target_size=None, grayscale=False):
+    image = keras_image_preprocessor.load_img(image_path,
+                                                grayscale,
+                                    target_size=target_size)
+    return keras_image_preprocessor.img_to_array(image)
+
+def preprocess_images2(image_array, backend='tensorflow'):
     if backend == 'tensorflow':
         # 'RGB'->'BGR'
         image_array = image_array[:, :, :, ::-1]
@@ -11,7 +22,7 @@ def preprocess_images(image_array, backend='tensorflow'):
     image_array[:, 2, :, :] -= 123.68
     return image_array
 
-def load_image(path, target_size=None):
+def load_image2(path, target_size=None):
     image = load_pil_image(path)
     image = resize_image(image, target_size)
     return image_to_array(image)
@@ -40,3 +51,9 @@ def image_to_array(image, backend='tensorflow'):
 def array_to_image(image_array, backend='tensorflow'):
     image_array = image_array.astype('uint8')
     return pil_image.fromarray(image_array, 'RGB')
+
+def get_image_size(path):
+    image = pil_image.open(path)
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    return image.size[::-1]
