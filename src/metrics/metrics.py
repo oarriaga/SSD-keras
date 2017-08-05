1dd172fc70dc5d36e668687725b534fded2fd4f3
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-08-05 17:39:12 +0200

    Added new mAP evaluation metric script

--- src/metrics/metrics.py
+++ src/metrics/metrics.py
@@ -30,3 +30,39 @@ def compute_average_precision(precision, recall):
     average_precision = np.sum(
             (recall[indices] - recall[indices - 1]) * precision[indices])
     return average_precision
+
+
+def compute_average_precision_2(prec, rec, use_07_metric=True):
+    """ ap = voc_ap(rec, prec, [use_07_metric])
+    Compute VOC AP given precision and recall.
+    If use_07_metric is true, uses the
+    VOC 07 11 point method (default:False).
+    """
+    if use_07_metric:
+        # 11 point metric
+        ap = 0.
+        for t in np.arange(0., 1.1, 0.1):
+            if np.sum(rec >= t) == 0:
+                p = 0
+            else:
+                p = np.max(prec[rec >= t])
+            ap = ap + p / 11.
+    else:
+        # correct AP calculation
+        # first append sentinel values at the end
+        mrec = np.concatenate(([0.], rec, [1.]))
+        mpre = np.concatenate(([0.], prec, [0.]))
+
+        # compute the precision envelope
+        for i in range(mpre.size - 1, 0, -1):
+            mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+        # to calculate area under PR curve, look for points
+        # where X axis (recall) changes value
+        i = np.where(mrec[1:] != mrec[:-1])[0]
+
+        # and sum (\Delta recall) * prec
+        ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
+
+
