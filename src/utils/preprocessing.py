1451edd55a743b3d1dd41ef6be6d1567d35c7a7c
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-08-09 17:53:39 +0200

    Started refactoring of modules

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -15,5 +15,6 @@ def load_image(image_path, target_size=None):
     image_array = cv2.imread(image_path)
     image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
     height, width = image_array.shape[:2]
-    image_array = cv2.resize(image_array, target_size)
+    if target_size is not None:
+        image_array = cv2.resize(image_array, target_size)
     return image_array, (height, width)
