d26818454eb3f6629301e167754ebb434595571f
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-10-02 15:21:23 +0200

    Started visualization file

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -15,13 +15,16 @@ def substract_mean(image_array):
     return image_array
 
 
-def load_image(image_path, target_size=None):
+def load_image(image_path, target_size=None, return_original_shape=False):
     image_array = cv2.imread(image_path)
     image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
     height, width = image_array.shape[:2]
     if target_size is not None:
         image_array = cv2.resize(image_array, target_size)
-    return image_array, (height, width)
+    if return_original_shape:
+        return image_array, (height, width)
+    else:
+        return image_array
 
 
 def get_image_size(image_path):
