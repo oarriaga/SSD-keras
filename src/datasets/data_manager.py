7f9ed5098ccc763e9da8b0749c07c714398a6b54
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-10-02 15:21:23 +0200

    Started visualization file

--- src/datasets/data_manager.py
+++ src/datasets/data_manager.py
@@ -8,13 +8,15 @@ class DataManager(object):
                  dataset_path='../datasets/VOCdevkit/'):
 
         self.dataset_name = dataset_name
-        self.dataset_path = dataset_path
-        self.with_difficult_objects = with_difficult_objects
         self.split = split
-
+        self.with_difficult_objects = with_difficult_objects
         self.class_names = class_names
         if class_names == 'all':
             self.class_names = get_class_names(self.dataset_name)
+        self.dataset_path = dataset_path
+        self.images_path = None
+        self.arg_to_class = None
+        self.ground_truth_data = None
 
     def load_data(self):
         if self.dataset_name == 'VOC2007':
@@ -27,4 +29,6 @@ class DataManager(object):
                                 self.class_names,
                                 self.with_difficult_objects,
                                 self.dataset_path)
+        self.images_path = self.parser.images_path
+        self.arg_to_class = self.parser.arg_to_class
         self.ground_truth_data = self.parser.load_data()
