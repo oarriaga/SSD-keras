f910813ffeffb982450c11dad128ea606d896620
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-10-18 14:39:16 +0200

    Added option to load image using RGB or BGR format

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -15,9 +15,11 @@ def substract_mean(image_array):
     return image_array
 
 
-def load_image(image_path, target_size=None, return_original_shape=False):
+def load_image(image_path, target_size=None,
+               return_original_shape=False, RGB=True):
     image_array = cv2.imread(image_path)
-    image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+    if RGB:
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
     height, width = image_array.shape[:2]
     if target_size is not None:
         image_array = cv2.resize(image_array, target_size)
