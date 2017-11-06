5ede10e5920de8dc5f013333f2baf12024a1b0de
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-11-06 14:15:40 +0100

    Added generator test

---
+++ src/generator_test.py
@@ -0,0 +1,36 @@
+from datasets.data_manager import DataManager
+from utils.boxes import create_prior_boxes
+from utils.inference import plot_box_data
+from utils.inference import get_colors
+from utils.boxes import unregress_boxes
+from utils.data_augmentation import ImageGenerator
+import matplotlib.pyplot as plt
+
+# parameters
+dataset_name = 'VOC2012'
+batch_size = 20
+colors = get_colors(25)
+prior_boxes = create_prior_boxes()
+
+# loading training data
+data_manager = DataManager(dataset_name, 'train')
+train_data = data_manager.load_data()
+arg_to_class = data_manager.arg_to_class
+# loading validation data
+val_data = DataManager(dataset_name, 'val').load_data()
+
+# generating output
+generator = ImageGenerator(train_data, val_data, prior_boxes, batch_size)
+generated_data = next(generator.flow('train'))
+transformed_image_batch = generated_data[0]['input_1']
+generated_output = generated_data[1]['predictions']
+
+for batch_arg, transformed_image in enumerate(transformed_image_batch):
+    positive_mask = generated_output[batch_arg, :, 4] != 1
+    regressed_boxes = generated_output[batch_arg]
+    unregressed_boxes = unregress_boxes(regressed_boxes, prior_boxes)
+    unregressed_positive_boxes = unregressed_boxes[positive_mask]
+    plot_box_data(unregressed_positive_boxes, transformed_image,
+                  arg_to_class, colors=colors)
+    plt.imshow(transformed_image.astype('uint8'))
+    plt.show()
