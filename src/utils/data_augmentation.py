8233d03b34d18c8709f13354815e2f557f07c586
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-11-06 10:19:40 +0100

    Added failed tested version from SSD

--- src/utils/data_augmentation.py
+++ src/utils/data_augmentation.py
@@ -8,28 +8,17 @@ from .augmentations import SSDAugmentation
 
 
 class ImageGenerator(object):
-    """ Image generator with saturation, brightness, lighting, contrast,
-    horizontal flip and vertical flip transformations. It supports
-    bounding boxes coordinates.
 
-    TODO:
-        - Finish preprocess_images method.
-        - Add random crop method.
-        - Finish support for not using bounding_boxes.
-    """
-    # change ground_truth_data to ground_truth_data
     def __init__(self, train_data, val_data, prior_boxes,
                  batch_size=32, box_scale_factors=[.1, .1, .2, .2],
                  num_classes=21):
 
-        # num_classes=21, path_prefix=None):
         self.train_data = train_data
         self.val_data = val_data
         self.num_classes = num_classes
         self.prior_boxes = prior_boxes
         self.box_scale_factors = box_scale_factors
         self.batch_size = batch_size
-        # self.path_prefix = path_prefix
         self.transform = SSDAugmentation(300, (B_MEAN, G_MEAN, R_MEAN))
 
     def flow(self, mode='train'):
@@ -49,18 +38,15 @@ class ImageGenerator(object):
         while True:
             inputs = []
             targets = []
-            for key in keys:
-                # image_path = self.path_prefix + key
-                image_path = key
+            for image_path in keys:
                 image_array = load_image(image_path, RGB=False).copy()
-                box_data = ground_truth_data[key].copy()
+                box_data = ground_truth_data[image_path].copy()
+
                 if mode == 'train':
                     data = (image_array, box_data[:, :4], box_data[:, 4:])
                     image_array, box_corners, labels = self.transform(*data)
-                    # print('box_corners:', box_corners)
-                    # print('labels:', labels)
                     box_data = np.concatenate([box_corners, labels], axis=1)
-                    # print('box_data:', box_data)
+
                 box_data = assign_prior_boxes(self.prior_boxes, box_data,
                                               self.num_classes,
                                               self.box_scale_factors)
