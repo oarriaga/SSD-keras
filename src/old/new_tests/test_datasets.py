50d8472cadc0fdcb6886bbe79e997e4c46ef656b
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-11-06 17:03:48 +0100

    Removed old files from actual repository

--- src/old/new_tests/test_datasets.py
+++
@@ -1,6 +0,0 @@
-from datasets import DataManager
-
-classes = ['background', 'dog']
-data_manager = DataManager('VOC2007', 'test', classes, False)
-data = data_manager.load_data()
-print(len(data.keys()))
