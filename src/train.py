45152e9761975a3adc5528c0e6ec431393af9fd6
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-11-07 15:55:18 +0100

    Fixed prior boxes encoding

--- src/train.py
+++ src/train.py
@@ -10,10 +10,13 @@ from models import SSD300
 from keras.optimizers import Adam
 from utils.generator import ImageGenerator
 from utils.boxes import create_prior_boxes
+from utils.boxes import to_point_form
 
 batch_size = 5
 num_epochs = 2
 image_shape = (300, 300, 3)
+box_scale_factors = [.1, .1, .2, .2]
+negative_positive_ratio = 3
 
 dataset_manager = DataManager(['VOC2007', 'VOC2012'], ['trainval', 'trainval'])
 train_data = dataset_manager.load_data()
@@ -22,8 +25,9 @@ class_names = dataset_manager.class_names
 num_classes = len(class_names)
 
 
-prior_boxes = create_prior_boxes()
-generator = ImageGenerator(train_data, val_data, prior_boxes, batch_size)
+prior_boxes = to_point_form(create_prior_boxes())
+generator = ImageGenerator(train_data, val_data, prior_boxes, batch_size,
+                           box_scale_factors, num_classes)
 
 weights_path = '../trained_models/SSD300_weights.hdf5'
 frozen_layers = ['input_1', 'conv1_1', 'conv1_2', 'pool1',
@@ -31,7 +35,8 @@ frozen_layers = ['input_1', 'conv1_1', 'conv1_2', 'pool1',
                  'conv3_1', 'conv3_2', 'conv3_3', 'pool3']
 
 model = SSD300(image_shape, num_classes, weights_path, frozen_layers, True)
-multibox_loss = MultiboxLoss(num_classes, neg_pos_ratio=2.0).compute_loss
+multibox_loss = MultiboxLoss(
+        num_classes, neg_pos_ratio=negative_positive_ratio).compute_loss
 model.compile(Adam(lr=3e-4), loss=multibox_loss)
 
 
@@ -48,7 +53,6 @@ early_stop = EarlyStopping(min_delta=1e-3, patience=25, verbose=1)
 log = CSVLogger(model_path + 'SSD_scratch.log')
 callbacks = [checkpoint, plateau, early_stop, log]
 
-# TODO fix mode to account also for val
 model.fit_generator(generator.flow(mode='train'),
                     steps_per_epoch=int(len(train_data) / batch_size),
                     epochs=num_epochs, verbose=1,
