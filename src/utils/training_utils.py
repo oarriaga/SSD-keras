24e9a7179d4b7494d58583c93bf146e21b44dda1
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-11-10 16:35:00 +0100

    Added better epoch scheduling

--- src/utils/training_utils.py
+++ src/utils/training_utils.py
@@ -2,6 +2,20 @@ def scheduler(epoch, decay=0.1, base_learning_rate=3e-3):
     return base_learning_rate * decay**(epoch)
 
 
+class Scheduler():
+    def __init__(self, scheduled_epochs=[80, 100, 120], decay=0.1,
+                 base_learning_rate=3e-3):
+        self.scheduled_epochs = scheduled_epochs
+        self.decay = decay
+        self.base_learning_rate = base_learning_rate
+
+    def schedule(self, epoch):
+        if epoch in self.scheduled_epochs:
+            self.base_learning_rate = (self.base_learning_rate *
+                                       (self.decay**(epoch)))
+        return self.base_learning_rate
+
+
 def split_data(ground_truths, training_ratio=.8):
     ground_truth_keys = sorted(ground_truths.keys())
     num_train = int(round(training_ratio * len(ground_truth_keys)))
