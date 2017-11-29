3e7640ba8648fd7a1355b76c14ec43bfcd251782
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-11-29 15:28:22 +0100

    Added new learning rate scheduler

--- src/utils/training_utils.py
+++ src/utils/training_utils.py
@@ -1,7 +1,35 @@
+"""
 class LearningRateManager():
-    def __init__(self, decay=0.94, learning_rate=3e-3):
+    def __init__(self, learning_rate=.001, decay=.1,
+                 step_values=[160, 190, 220]):
         self.decay = decay
         self.learning_rate = learning_rate
+        self.step_values = step_values
 
     def schedule(self, epoch):
+        if epoch in self.step_values:
+            self.learning_rate = self.learning_rate * .1
         return self.learning_rate * (self.decay**(epoch))
+"""
+
+
+class LearningRateManager():
+    def __init__(self, learning_rate=.001, decay=.1,
+                 step_values=[160, 190, 220]):
+        self.learning_rate = learning_rate
+        self.decay = decay
+        self.step_values = step_values
+
+    def schedule(self, epoch):
+        if epoch in self.step_values:
+            self.learning_rate = self.learning_rate * self.decay
+        return self.learning_rate
+
+
+"""
+learning_rate_manager = LearningRateManager()
+for epoch in range(250):
+    print(epoch)
+    print('sch', learning_rate_manager.schedule(epoch))
+    print('lr', learning_rate_manager.learning_rate)
+"""
