f8dfa188f724bf4e1bde522604aa8a0db67af66d
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-12-05 10:55:48 +0100

    Recovered old scheduling function

--- src/utils/training_utils.py
+++ src/utils/training_utils.py
@@ -12,7 +12,7 @@ class LearningRateManager():
         return self.learning_rate * (self.decay**(epoch))
 """
 
-
+"""
 class LearningRateManager():
     def __init__(self, learning_rate=.001, decay=.1,
                  step_values=[160, 190, 220]):
@@ -25,11 +25,17 @@ class LearningRateManager():
             self.learning_rate = self.learning_rate * self.decay
         return self.learning_rate
 
-
-"""
 learning_rate_manager = LearningRateManager()
 for epoch in range(250):
     print(epoch)
     print('sch', learning_rate_manager.schedule(epoch))
     print('lr', learning_rate_manager.learning_rate)
 """
+
+class LearningRateManager():
+    def __init__(self, learning_rate, decay=0.98):
+        self.decay = decay
+        self.learning_rate = learning_rate
+
+    def schedule(self, epoch):
+        return self.learning_rate * (self.decay**(epoch))
