a8212b0aaf4047e21f174d978ec23c1f29f328ef
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-12-05 11:56:57 +0100

    Updated training script

--- src/utils/training_utils.py
+++ src/utils/training_utils.py
@@ -30,7 +30,6 @@ for epoch in range(250):
     print(epoch)
     print('sch', learning_rate_manager.schedule(epoch))
     print('lr', learning_rate_manager.learning_rate)
-"""
 
 class LearningRateManager():
     def __init__(self, learning_rate, decay=0.98):
@@ -39,3 +38,25 @@ class LearningRateManager():
 
     def schedule(self, epoch):
         return self.learning_rate * (self.decay**(epoch))
+
+"""
+
+
+class LearningRateManager():
+    def __init__(self, learning_rate, decay=0.1, step_epochs=None):
+        self.decay = decay
+        self.learning_rate = learning_rate
+        self.step_epochs = step_epochs
+
+    def schedule(self, epoch):
+        if epoch in self.step_epochs:
+            self.learning_rate = self.learning_rate * self.decay
+        return self.learning_rate
+
+
+"""
+
+lr_manager = LearningRateManager(1e-3, .1, [80, 100, 120])
+for epoch in range(140):
+    print(epoch, lr_manager.schedule(epoch))
+"""
