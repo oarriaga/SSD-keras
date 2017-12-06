e6e9552c7bad94d49c1bd1ccfaafef4040bd0b79
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-12-06 17:11:32 +0100

    Divided training model utils

--- src/utils/training_utils.py
+++ src/utils/training_utils.py
@@ -1,48 +1,5 @@
 from keras.callbacks import Callback
 
-"""
-class LearningRateManager():
-    def __init__(self, learning_rate=.001, decay=.1,
-                 step_values=[160, 190, 220]):
-        self.decay = decay
-        self.learning_rate = learning_rate
-        self.step_values = step_values
-
-    def schedule(self, epoch):
-        if epoch in self.step_values:
-            self.learning_rate = self.learning_rate * .1
-        return self.learning_rate * (self.decay**(epoch))
-"""
-
-"""
-class LearningRateManager():
-    def __init__(self, learning_rate=.001, decay=.1,
-                 step_values=[160, 190, 220]):
-        self.learning_rate = learning_rate
-        self.decay = decay
-        self.step_values = step_values
-
-    def schedule(self, epoch):
-        if epoch in self.step_values:
-            self.learning_rate = self.learning_rate * self.decay
-        return self.learning_rate
-
-learning_rate_manager = LearningRateManager()
-for epoch in range(250):
-    print(epoch)
-    print('sch', learning_rate_manager.schedule(epoch))
-    print('lr', learning_rate_manager.learning_rate)
-
-class LearningRateManager():
-    def __init__(self, learning_rate, decay=0.98):
-        self.decay = decay
-        self.learning_rate = learning_rate
-
-    def schedule(self, epoch):
-        return self.learning_rate * (self.decay**(epoch))
-
-"""
-
 
 class LearningRateManager():
     def __init__(self, learning_rate, decay=0.1, step_epochs=None):
@@ -63,4 +20,4 @@ class MultiGPUModelCheckpoint(Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         val_loss = logs['val_loss']
-        self.cpu_model.save(self.save_path.format(str(epoch), str(val_loss)))
+        self.cpu_model.save(self.save_path.format(epoch, val_loss))
