9e785f9a7eac862dc7c08b6dc13a5f7a719cd108
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-12-30 13:47:19 +0100

    Added Keras categorical crossentropy and refactored variable names for easier understanding

--- src/models/experimental_loss.py
+++ src/models/experimental_loss.py
@@ -1,15 +1,16 @@
-import tensorflow as tf
 import keras.backend as K
+import tensorflow as tf
 
 
 class MultiboxLoss(object):
     def __init__(self, num_classes, neg_pos_ratio=3, batch_size=32,
-                 alpha=1.0, background_id=0):
+                 alpha=1.0, background_id=0, max_num_negatives=300):
         self.num_classes = num_classes
         self.alpha = alpha
         self.neg_pos_ratio = neg_pos_ratio
         self.background_id = background_id
         self.batch_size = batch_size
+        self.max_num_negatives = max_num_negatives
 
     def smooth_l1(self, y_true, y_pred):
         absolute_value_loss = K.abs(y_true - y_pred) - 0.5
@@ -20,18 +21,17 @@ class MultiboxLoss(object):
         return K.sum(l1_smooth_loss, axis=-1)
 
     def cross_entropy(self, y_true, y_pred):
-        y_pred /= tf.reduce_sum(y_pred, axis=-1, keep_dims=True)
+        # y_pred /= tf.reduce_sum(y_pred, axis=-1, keep_dims=True)
         y_pred = K.maximum(K.minimum(y_pred, 1 - 1e-15), 1e-15)
         cross_entropy_loss = - K.sum(y_true * K.log(y_pred), axis=-1)
         return cross_entropy_loss
 
     def compute_loss(self, y_true, y_pred):
 
-        class_loss = self.cross_entropy(y_true[:, :, 4:], y_pred[:, :, 4:])
-        """
+        # class_loss = self.cross_entropy(y_true[:, :, 4:], y_pred[:, :, 4:])
         class_loss = K.categorical_crossentropy(y_true[:, :, 4:],
                                                 y_pred[:, :, 4:])
-        """
+
         # return K.concatenate([class_loss, class_loss_old], axis=0)
         local_loss = self.smooth_l1(y_true[:, :, :4], y_pred[:, :, :4])
         negative_mask = y_true[:, :, 4 + self.background_id]
@@ -45,28 +45,30 @@ class MultiboxLoss(object):
 
         # obtaining the number of negatives in the batch
         num_positives_per_sample = K.cast(K.sum(positive_mask, -1), 'int32')
-        num_negatives_per_sample = K.cast(K.sum(negative_mask, -1), 'int32')
-        num_negatives_in_batch = K.sum(num_negatives_per_sample)
+        # num_negatives_per_sample = K.cast(K.sum(negative_mask, -1), 'int32')
+        # num_negatives_in_batch = K.sum(num_negatives_per_sample)
         num_hard_negatives = self.neg_pos_ratio * num_positives_per_sample
-        num_negatives = K.minimum(num_hard_negatives, num_negatives_in_batch)
-        all_negative_class_losses = class_loss * negative_mask
+        num_negatives_per_sample = K.minimum(num_hard_negatives,
+                                             self.max_num_negatives)
+        negative_class_losses = class_loss * negative_mask
 
         negative_class_loss = []
-        for batch_arg in range(self.batch_size):
-            sample_num_negatives = num_negatives[batch_arg]
-            all_negative_sample_loss = all_negative_class_losses[batch_arg]
-            negative_sample_losses = tf.nn.top_k(all_negative_sample_loss,
-                                                 k=sample_num_negatives,
-                                                 sorted=True)[0]
-            negative_sample_loss = K.sum(negative_sample_losses)
+        for sample_arg in range(self.batch_size):
+            num_negatives_in_sample = num_negatives_per_sample[sample_arg]
+            negative_sample_loss = negative_class_losses[sample_arg]
+            selected_negative_sample_losses = tf.nn.top_k(
+                                    negative_sample_loss,
+                                    k=num_negatives_in_sample,
+                                    sorted=True)[0]
+            negative_sample_loss = K.sum(selected_negative_sample_losses)
             negative_sample_loss = K.expand_dims(negative_sample_loss, -1)
             negative_class_loss.append(negative_sample_loss)
         negative_class_loss = K.concatenate(negative_class_loss)
-        return negative_class_loss
 
         class_loss = positive_class_loss + negative_class_loss
         total_loss = class_loss + (self.alpha * positive_local_loss)
 
+        # when the number of positives is zero set the total loss to zero
         batch_mask = K.not_equal(num_positives_per_sample, 0)
         total_loss = tf.where(batch_mask, total_loss, K.zeros_like(total_loss))
 
