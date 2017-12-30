010b573141c08dc14ce8e9b08e1f44c2d7fb0cad
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-12-30 14:42:53 +0100

    Added training script with sequencer instead of generator

--- src/train.py
+++ src/train.py
@@ -7,21 +7,23 @@ from datasets import DataManager
 from models.experimental_loss import MultiboxLoss
 from models import SSD300
 from keras.optimizers import SGD
-from utils.generator import ImageGenerator
+# from utils.generator import ImageGenerator
+from utils.sequencer_manager import SequenceManager
 from utils.boxes import create_prior_boxes
 from utils.boxes import to_point_form
 from utils.training_utils import LearningRateManager
 
 # hyper-parameters
-batch_size = 32
+batch_size = 3
 num_epochs = 233
 image_shape = (300, 300, 3)
 box_scale_factors = [.1, .1, .2, .2]
 negative_positive_ratio = 3
-learning_rate = 1e-3
+learning_rate = 1e-4
 weight_decay = 5e-4
 momentum = .9
 optimizer = SGD(learning_rate, momentum, decay=weight_decay)
+# optimizer = 'adam'
 decay = 0.1
 step_epochs = [154, 193, 232]
 randomize_top = True
@@ -45,8 +47,11 @@ val_data = val_data_manager.load_data()
 
 # generator
 prior_boxes = to_point_form(create_prior_boxes())
-generator = ImageGenerator(train_data, val_data, prior_boxes, batch_size,
-                           box_scale_factors, num_classes)
+train_sequencer = SequenceManager(train_data, 'train', prior_boxes,
+                                  batch_size, box_scale_factors, num_classes)
+
+val_sequencer = SequenceManager(val_data, 'val', prior_boxes,
+                                batch_size, box_scale_factors, num_classes)
 
 # model
 multibox_loss = MultiboxLoss(num_classes, negative_positive_ratio, batch_size)
@@ -64,13 +69,13 @@ learning_rate_schedule = LearningRateScheduler(learning_rate_manager.schedule)
 callbacks = [checkpoint, log, learning_rate_schedule]
 
 # model fit
-model.fit_generator(generator.flow(mode='train'),
+model.fit_generator(train_sequencer,
                     steps_per_epoch=int(len(train_data) / batch_size),
                     epochs=num_epochs,
                     verbose=1,
                     callbacks=callbacks,
-                    validation_data=generator.flow(mode='val'),
+                    validation_data=val_sequencer,
                     validation_steps=int(len(val_data) / batch_size),
-                    # use_multiprocessing=True,
+                    use_multiprocessing=False,
                     max_queue_size=7,
                     workers=5)
