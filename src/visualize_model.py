76796c823173d4355714d071c0372dc2ab00cc04
Octavio Arriaga <arriaga.camargo@gmail.com>
2018-03-13 11:08:10 +0100

    Added pre-trained modified VGG16 weigths

--- src/visualize_model.py
+++ src/visualize_model.py
@@ -1,10 +1,23 @@
 from utils.visualizations import plot_kernels
 import matplotlib.pyplot as plt
+# from models.ssd_utils import construct_SSD
 from models import SSD300
 
+
+"""
 weights_path = '../trained_models/SSD300_weights.hdf5'
 model = SSD300(weights_path=weights_path)
 model.summary()
+kernel_weights = model.get_layer('conv2d_1').get_weights()[0]
+plot_kernels(kernel_weights[:, :, 0:1, :])
+plot_kernels(kernel_weights[:, :, 1:2, :])
+plot_kernels(kernel_weights[:, :, 2:3, :])
+plt.show()
+"""
+
+weights_path = '../trained_models/VGG16_weights.hdf5'
+model = SSD300()
+model.load_weights(weights_path, by_name=True)
 kernel_weights = model.get_layer('conv2d_1').get_weights()[0]
 plot_kernels(kernel_weights[:, :, 0:1, :])
 plot_kernels(kernel_weights[:, :, 1:2, :])
