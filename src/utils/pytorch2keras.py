dcf42158127bd09d7824ece656ecdbf1313023d3
Octavio Arriaga <arriaga.camargo@gmail.com>
2018-03-13 11:08:10 +0100

    Added pre-trained modified VGG16 weigths

--- src/utils/pytorch2keras.py
+++ src/utils/pytorch2keras.py
@@ -1,8 +1,9 @@
 import torch
 import numpy as np
-from models.modified_VGG import modified_VGG16
+from models import SSD300
 
-weights_path = 'vgg16_reducedfc.pth'
+
+weights_path = '../trained_models/vgg16_reducedfc.pth'
 vgg_weights = torch.load(weights_path)
 
 biases = []
@@ -12,21 +13,23 @@ for name, torch_weights in vgg_weights.items():
     if 'bias' in name:
         biases.append(torch_weights.numpy())
     if 'weight' in name:
+        print(torch_weights.numpy().shape)
         conv_weights = torch_weights.numpy()
         conv_weights = np.rollaxis(conv_weights, 0, 4)
         conv_weights = np.rollaxis(conv_weights, 0, 3)
         weights.append(conv_weights)
 vgg_weights = list(zip(weights, biases))
-model = modified_VGG16()
 
+base_model = SSD300(return_base=True)
 pytorch_layer_arg = 0
-for layer in model.layers[1:]:
+for layer in base_model.layers[1:]:
     conv_weights = vgg_weights[pytorch_layer_arg][0]
     bias_weights = vgg_weights[pytorch_layer_arg][1]
-    if 'pool' not in layer.name:
+    if ('conv2d' in layer.name) or ('branch_2' in layer.name):
+        print(layer.name)
         print('pre-trained_weigths:', conv_weights.shape)
         print('model weights:', layer.get_weights()[0].shape)
         layer.set_weights([conv_weights, bias_weights])
         pytorch_layer_arg = pytorch_layer_arg + 1
 
-model.save_weights('VGG16_weights.hdf5')
+base_model.save_weights('../trained_models/VGG16_weights.hdf5')
