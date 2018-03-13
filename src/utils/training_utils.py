e0ee28fa79bb4580e1a132e71b6fb7598ba1532a
Octavio Arriaga <arriaga.camargo@gmail.com>
2018-03-13 14:46:06 +0100

    Added learning rate manager

--- src/utils/training_utils.py
+++ src/utils/training_utils.py
@@ -1,18 +1,6 @@
 from keras.callbacks import Callback
 
 
-class LearningRateManager():
-    def __init__(self, learning_rate, decay=0.1, step_epochs=None):
-        self.decay = decay
-        self.learning_rate = learning_rate
-        self.step_epochs = step_epochs
-
-    def schedule(self, epoch):
-        if epoch in self.step_epochs:
-            self.learning_rate = self.learning_rate * self.decay
-        return self.learning_rate
-
-
 class MultiGPUModelCheckpoint(Callback):
     def __init__(self, save_path, cpu_model):
         self.save_path = save_path
@@ -21,3 +9,15 @@ class MultiGPUModelCheckpoint(Callback):
     def on_epoch_end(self, epoch, logs=None):
         val_loss = logs['val_loss']
         self.cpu_model.save(self.save_path.format(epoch, val_loss))
+
+
+class LearningRateManager():
+    def __init__(self, learning_rate, gamma_decay, scheduled_epochs):
+        self.learning_rate = learning_rate
+        self.gamma_decay = gamma_decay
+        self.scheduled_epochs = scheduled_epochs
+
+    def schedule(self, epoch):
+        if epoch in self.scheduled_epochs:
+            self.learning_rate = self.learning_rate * self.gamma_decay
+        return self.learning_rate
