551d1b64d450537b8fdded1b60200b71afc1bb2e
oarriaga <arriaga.camargo@gmail.com>
2018-05-14 09:45:56 +0200

    Added a single script for downloading VOC2007 and VOC2012 datasets

--- datasets/VOC0712_downloader.sh
+++ datasets/VOC0712_downloader.sh
@@ -23,3 +23,25 @@ end=`date +%s`
 runtime=$((end-start))
 
 echo "Completed in" $runtime "seconds"
+
+
+start=`date +%s`
+
+# handle optional download dir
+
+echo "Downloading VOC2012 trainval ..."
+# Download the data.
+curl -LO http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+echo "Done downloading."
+
+
+# Extract data
+echo "Extracting trainval ..."
+tar -xvf VOCtrainval_11-May-2012.tar
+echo "removing tar ..."
+rm VOCtrainval_11-May-2012.tar
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "Completed in" $runtime "seconds"
